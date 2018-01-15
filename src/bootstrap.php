6e558cd7ae93d5897509c4324c5d3d206e7019e9
Cody Erekson <cody.erekson@gmail.com>
2018-01-15 09:38:28 -0700

    Use proper function syntax for includes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -12,25 +12,29 @@ if (PHP_SAPI == 'cli-server') {
 	}
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require(__DIR__ . '/../vendor/autoload.php');
 
+// Get global functions
+require(__DIR__ . '/../src/functions.php');
+
+// Maybe don't need this if using JWT for entirely stateless API
 session_start();
 
 $env = new \Dotenv\Dotenv(realpath(__DIR__ . '/../'));
 $env->load();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require(__DIR__ . '/../src/settings.php');
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require(__DIR__ . '/../src/dependencies.php');
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require(__DIR__ . '/../src/middleware.php');
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require(__DIR__ . '/../src/routes.php');
 
 $app->run();
 

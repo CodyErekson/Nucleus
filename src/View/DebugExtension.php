c9fcd0cd8905302ee0eaa292ad3ef26ee5d700e0
Cody Erekson <cody.erekson@gmail.com>
2018-01-15 14:20:15 -0700

    Add dBug to Twig debug extension

--- src/View/DebugExtension.php
+++ src/View/DebugExtension.php
@@ -10,17 +10,38 @@ class DebugExtension extends \Twig_Extension
 	{
 		return [
 			new \Twig_SimpleFunction('d', [$this, 'dump']),
-			new \Twig_SimpleFunction('dd', [$this, 'dumpAndDie'])
+			new \Twig_SimpleFunction('dd', [$this, 'dumpAndDie']),
+			new \Twig_SimpleFunction('ddd', [$this, 'dBug'])
 		];
 	}
 
+	/**
+	 * Dump variable to screen
+	 * @param $var
+	 * @return string
+	 */
 	public function dump($var)
 	{
 		return d($var);
 	}
 
+	/**
+	 * Dump variable and cease execution
+	 * @param $var
+	 * @return string
+	 */
 	public function dumpAndDie($var)
 	{
 		return dd($var);
+	}
+
+	/**
+	 * Use dBug to output variables -- right now this only works with scalar variables
+	 * @param $var
+	 * @return string
+	 */
+	public function dBug($var)
+	{
+		return ddd($var);
 	}
 }

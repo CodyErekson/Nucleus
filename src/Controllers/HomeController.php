4bd105ff825977f592258fb746f2394861aab468
Cody Erekson <cody.erekson@gmail.com>
2018-01-15 09:49:11 -0700

    Playing around

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -6,7 +6,7 @@ class HomeController extends BaseController
 {
 	public function home($request, $response, $args)
 	{
-		$this->container['debug.log']->debug("hello " . $args['name']);
+		$this->container['debug.log']->debug("hello " . print_r($args));
 		return $this->container->view->render($response, 'index.html', $args);
 	}
 }

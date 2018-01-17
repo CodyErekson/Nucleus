2cc8bc83828c84187961276f8cb42f153fea09cc
Cody Erekson <cody.erekson@gmail.com>
2018-01-17 10:16:29 -0700

    Formatting

--- src/Middleware/CsrfCheckMiddleware.php
+++ src/Middleware/CsrfCheckMiddleware.php
@@ -7,8 +7,6 @@ class CsrfCheckMiddleware extends BaseMiddleware
 
 	public function __invoke($request, $response, $next)
 	{
-
-
 		if (false === $request->getAttribute('csrf_status')) {
 			// display suitable error here
 			$this->container->flash->addMessage('error', 'Form security timed out. Please try again.');

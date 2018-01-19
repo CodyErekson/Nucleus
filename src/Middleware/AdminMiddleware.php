ee644f4ee3409ad76093324c6d2a8b3e57bc4926
Cody Erekson <cody.erekson@gmail.com>
2018-01-18 17:02:35 -0700

    Rearrange a few files

--- src/Middleware/AdminMiddleware.php
+++ src/Middleware/AdminMiddleware.php
@@ -7,10 +7,15 @@ class AdminMiddleware extends BaseMiddleware
 
 	public function __invoke($request, $response, $next)
 	{
-
-		if ( $this->container->auth->getRole() != "admin" ){
+		$roles = $this->container->user_manager->currentUser()->getRoles();
+		$this->container['debug.log']->debug("Roles", $roles);
+		if ( in_array("admin", $roles) ){
 			$this->container->flash->addMessage('error', 'This page is only available to administrators.');
-			return $response->withRedirect($this->container->router->pathFor('home'));
+			$res = $response->withHeader("Content-Type", "application/json");
+			$res = $res->withStatus(401);
+			$res->getBody()->write(json_encode(['error' => 'This page is only available to administrators.']));
+			return $res;
+			//return $response->withRedirect($this->container->router->pathFor('home'));
 		}
 
 		$response = $next($request, $response);
